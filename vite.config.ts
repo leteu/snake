94d1309bc66ff56c368484bc807d5cfe871a0831
ghshin <leteu610@gmail.com>
2022-03-14 15:18:37 +0900

    파일 경로 alias 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.ELECTRON=="true" ? './' : "./"
+  base: process.env.ELECTRON=="true" ? './' : "./",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'app': path.resolve(__dirname, './'),
+    }
+  }
 })
