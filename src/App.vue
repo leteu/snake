36da8b258dc8adc6a808bd75e87fc4b8c912ea7d
ghshin <leteu610@gmail.com>
2022-03-15 11:50:20 +0900

    둥근모꼴, 도스고딕 폰트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <Snake />
+  <span class="press-start-2p dung-geun-mo dos-gothic">&nbsp;</span>
 </template>
 
 <script lang="ts">
@@ -20,4 +21,22 @@ export default defineComponent({
   @font-face
     font-family: 'Press Start 2P'
     src: url('font/PressStart2P-Regular.ttf') format('truetype')
+
+  @font-face
+    font-family: '둥근모꼴'
+    src: url('font/DungGeunMo.ttf') format('truetype')
+
+  @font-face
+    font-family: 'DOSGothic'
+    src: url('font/DOSGothic.ttf') format('truetype')
+
+  .press-start-2p
+    font-family: 'Press Start 2P'
+  .dung-geun-mo
+    font-family: '둥근모꼴'
+  .dos-gothic
+    font-family: 'DOSGothic'
+
+  #app
+    overflow: hidden
 </style>
